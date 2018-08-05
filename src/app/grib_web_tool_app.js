f7ec04ec87e499e0405fd1a4dc30931f762cbfd5
perillaroc <perillaroc@gmail.com>
2018-08-05 20:46:09 +0800

    add color for title using styled-component.

--- src/app/grib_web_tool_app.js
+++ src/app/grib_web_tool_app.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import ScanningModePage from '../pages/scanning_mode/scanning_mode_page';
 
 import { Layout } from 'antd';
 
 const { Header, Footer, Sider, Content } = Layout;
 
+const AppTitle = styled.h1`
+   color: white
+`;
+
 
 export default class GribWebToolApp extends React.Component {
   render(){
+
     return (
       <Layout>
         <Header>
-          <h1>GRIB Web Tool</h1>
+          <AppTitle>GRIB Web Tool</AppTitle>
         </Header>
         <Content>
           <ScanningModePage/>
